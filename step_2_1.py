5fb748f86547c5af584f9a615ba0bdd4a555cb88
myeonghwi <automaster5013@gmail.com>
2026-04-03 11:44:15 +0900

    python_데이터_준비_소스코드

--- step_2_1.py
+++ step_2_1.py
@@ -5,7 +5,7 @@ df_raw = pd.read_excel(IN_DIR / "2024년1월.xlsx",
                         sheet_name="Sheet1", usecols="B:E", skiprows=2)
 df_raw
 
-
+#############################################(데이터_준비_소스코드)
 
 
 
